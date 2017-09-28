c0d6bc5eb776ea81ef34b59f5979f307ce6ac1fd
Thach Nguyen <dejokuondi@users.noreply.github.com>
2017-09-28 13:02:31 +0700

    Update default.rb

--- aus/recipes/default.rb
+++ aus/recipes/default.rb
@@ -5,13 +5,12 @@
   end
 end
 
-
-# Replace php.ini file.
-template '/etc/php5/apache2/php.ini' do
-  source "php.ini"
-end
-
 # Replace apache2.conf file.
 template '/etc/apache2/apache2.conf' do
   source "apache2.conf"
+end
+
+# Replace index.html file.
+template '/var/www/html/' do
+  source "stg/index.html"
 end
